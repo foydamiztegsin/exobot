f5ded87822d20c8122cd367f99c1a03fe9e0ef7b
foydamiztegsin <121927950+foydamiztegsin@users.noreply.github.com>
2023-01-06 17:30:53 +0500

    Create exobot.py

---
+++ exobot.py
@@ -0,0 +1,33 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5900347880:AAHXX302UiHzCELz5rlfWuwrKATpxY0pieE'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Assalomu alaykum!\nUshbu bot bepul serverga yuklash maqsadida test uchun yozildi!")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
