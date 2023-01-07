5e8371088b0b5a93db7295a7dbaf34acbb42295a
foydamiztegsin <121927950+foydamiztegsin@users.noreply.github.com>
2023-01-07 20:39:01 +0500

    Update exobot.py

--- exobot.py
+++ exobot.py
@@ -7,16 +7,13 @@ API_TOKEN = 'Botingiz Tokenini joylashtirasiz!'
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
+
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
     await message.reply("Assalomu alaykum!\nUshbu bot bepul serverga yuklash maqsadida test uchun yozildi!")
 
 
